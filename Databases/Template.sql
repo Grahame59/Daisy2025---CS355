c0bcae916277bfb8a282c43b9ce8be0cbc274bb2
Tanvir <tanvir@Tanvirs-MacBook-Air-2.local>
2025-04-08 12:59:28 -0400

    User Table Created

--- Databases/Template.sql
+++ Databases/Template.sql
@@ -1,5 +1,12 @@
 /* -----------------------------------------------------------------------------------------------------------------------------------------
                                                         Template File for SQL
   ---------------------------------------------------------------------------------------------------------------------------------------- */
+CREATE TABLE Users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    user_type ENUM('employee', 'member') NOT NULL
+);
 
-  
+CREATE TABLE Employee  
