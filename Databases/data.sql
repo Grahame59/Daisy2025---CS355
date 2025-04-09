69387b36b0406bf37d163325654566efd1d2e0e0
Tanvir <tanvir@Tanvirs-MacBook-Air-2.local>
2025-04-09 12:00:44 -0400

    Added new file will work more to add employee etc

---
+++ Databases/data.sql
@@ -0,0 +1,18 @@
+/* -------------------------------------------------------------------------
+                            File for data to insert for the tables 
+  -------------------------------------------------------------------------- */
+INSERT INTO Account (UserName, Password, EmailAddress, AccountType) VALUES 
+('alexsmith', 'Pass1234', 'alex.smith@example.com', 'Member'),
+('sarajones', 'SaraPass1', 'sara.jones@example.com', 'Employee'),
+('davidcho', 'ChoSecure9', 'david.cho@example.com', 'Supervisor'),
+('emilywang', 'EmWang88', 'emily.wang@example.com', 'Member'),
+('johnnyd', 'JDpass2024', 'johnny.d@example.com', 'Employee'),
+('nataliek', 'NatKey77', 'natalie.k@example.com', 'Member'),
+('michaelb', 'MikeB321', 'michael.b@example.com', 'Supervisor'),
+('ashleyt', 'AshPass45', 'ashley.t@example.com', 'Member'),
+('brianlee', 'BLeePass9', 'brian.lee@example.com', 'Employee'),
+('chloer', 'ChloeR2025', 'chloe.r@example.com', 'Supervisor'),
+('marcusb', 'MarcusB17', 'marcus.b@example.com', 'Member'),
+('jenniferk', 'JKpass890', 'jennifer.k@example.com', 'Employee'),
+('stephw', 'StephW22', 'stephanie.w@example.com', 'Member'),
+('toml', 'TomLpass!', 'tom.l@example.com', 'Employee');
